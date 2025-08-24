ad4ca56263f2cf294ecce798a80ea6dbaaef1f8b
mdrazaulkabir <razaulkabirit@gmail.com>
2025-08-24 22:54:47 +0600

    added all file initially

---
+++ lib/match_list_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class MatchListScreen extends StatefulWidget {
+  const MatchListScreen({super.key});
+
+  @override
+  State<MatchListScreen> createState() => _MatchListScreenState();
+}
+
+class _MatchListScreenState extends State<MatchListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+    );
+  }
+}
